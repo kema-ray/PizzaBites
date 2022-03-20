e4c637577c6e054697477223598d9b36eb20cb3a
kema-ray <rachelkemuma99@gmail.com>
2022-03-20 15:47:16 +0300

    add an event listener to button order

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function Getpizza (size,name,crust,toppings,totals){
     this.toppings = toppings;
     this.totals = totals;
 }
+$(document).ready(function(){
+    $("button.order").click(function(event){
+        let psize = $("#size option:selected").val();
+        let pname = $(".name option:selected").val();
+        let pcrust = $("#crust option:selected").val();
+        let ptopping = [];
+        $.each($("input[name='toppings']:checked"), function(){
+            ptopping.push($(this).val());
+        });
+        console.log(ptoppping.join(", "));
+    });
+});
