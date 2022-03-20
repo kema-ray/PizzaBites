a54624f37e67f89ce571a2db92f2a3243dd07b35
kema-ray <rachelkemuma99@gmail.com>
2022-03-20 16:19:25 +0300

    add case statements

--- script.js
+++ script.js
@@ -21,5 +21,42 @@ $(document).ready(function(){
             ptopping.push($(this).val());
         });
         console.log(ptoppping.join(", "));
+
+        switch(pcrust) {
+            case "0":
+                crust_price = 0;
+                break;
+                case "Cheese-stuffed":
+                    crust_price = 250;
+                    break;
+                    case "Crispy":
+                        crust_price = 180;
+                        break;
+                        case "Gluten-free":
+                            crust_price = 200;
+                            break;
+                            default:
+                                console.log("No price");
+        }
+        switch(psize) {
+            case "0":
+                price = 0;
+                break;
+                case "small":
+                    price = 700;
+                    console.log(price);
+                    break;
+                    case "medium":
+                        price = 850;
+                        console.log(price);
+                        break;
+                        case "large":
+                            price = 1000;
+                            console.log(price);
+                            break;
+                            default:
+                                console.log("fail");
+        }
+        event.preventDefault();
     });
 });
